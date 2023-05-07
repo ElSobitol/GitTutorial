43bd8512341b8b4e3b6551eca0edf612335291df
Макей 😈 <imac_makey@iMac-Makej.local>
2023-05-07 17:46:02 +0300

    homework_7

---
+++ homework_7.cs
@@ -0,0 +1,128 @@
+/* Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+*/
+
+
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+/* Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
+
+
+/* Задача 52: Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
